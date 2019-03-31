7d2c215d94dd2d3c899fa421ec809643e5bcf0a4
Tania Ali <taniafali@gmail.com>
2019-03-30 17:00:31 -0700

    Complete 'nonrepeatedCharacter" solution

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,27 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  // first filter out the non repeated characters into an array.....
+  // define an object that holds  all characters in the string as its properties and their counts as value.
+  var charCounts = {};
+  for (var i = 0; i < string.length; i++) {
+    if (charCounts.hasOwnProperty(string[i])) {
+      charCounts[string[i]]++;
+    } else {
+      charCounts[string[i]] = 1;
+    }
+  }
+ // filter out the non repeated charactors
+  var nonRepeatedChars = [];
+  for (var char in charCounts) {
+    if (charCounts[char] === 1) {
+      nonRepeatedChars.push(char);
+    }
+  }
+  //To get the first non repeat char, compare their indices ....
+  var firstNonRepeatedChar = nonRepeatedChars.reduce(function(acc, item) {
+    return (string.split().indexOf(item) < string.split().indexOf(acc))? item : acc;
+  },nonRepeatedChars[0]);
+  // return char with the lowest index.
+  return firstNonRepeatedChar;
 };
