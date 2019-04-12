ad9bf851228e147ac1b30ab05553ae50cf6c996f
Tania Ali <taniafali@gmail.com>
2019-04-11 21:26:49 -0700

    Complete toy problem 'commonCharacters' with extra credit

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,26 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+
+
+  // initialize a variable
+  var commonChars = "";
+  // convert string1 to an array to compare each of its letters with the other strings
+  var stringToCompare = string1.split("");
+  //initialize a variable to hold other strings to be compared with string1
+  var args= [...arguments].slice(1);
+  // iterate over string1 array
+  stringToCompare.forEach(function(item) {
+    // iterate over other strings  to compare with string1 characters one by one and check if the strings contain the string1 character
+    wasFound = args.every(function(string) {
+      return string.includes(item);
+
+    });
+    //check if all other strings contain string1 character  then concatenate the string1 character into commonChars variable
+    if(wasFound && !commonChars.includes(item) && item !== " ") {
+      commonChars+=item;
+    }
+  });
+  // provide commonChars as the operation's result
+  return commonChars
 };
