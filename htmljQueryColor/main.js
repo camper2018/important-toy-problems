19f63b87f07974d14607e25c7cb58b0f4581b829
Tania Ali <taniafali@gmail.com>
2019-04-27 16:22:40 -0700

    Add solution to  htmljQueryColor

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,11 +6,27 @@ $(function() {
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
-
-
+  $('p').each((i, p)=> {
+    var words = $(p).text().split(' ');
+    var content = '';
+    words.forEach(word => {
+      console.log(word);
+      content+= `<span>${word}</span>  `;
+    });
+    $(p).html(content);
+  });
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+
+  $('span').each((i, span)=> {
+    setInterval(()=> {
+      var r = Math.floor( Math.random() * 256 );
+      var g = Math.floor( Math.random() * 256 );
+      var b = Math.floor( Math.random() * 256 );
+      $(span).css({ backgroundColor: `rgb(${r}, ${g}, ${b})` });
+    }, 1000);
+  });
 
 });
