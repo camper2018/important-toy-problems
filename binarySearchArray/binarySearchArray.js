d7611217e7b624c5fea8056ac31d9f2261089dc9
Tania Ali <taniafali@gmail.com>
2019-05-03 16:50:02 -0700

    Add solution to 'binarySearchArray' toy problem

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,31 @@
  */
 
 var binarySearch = function (array, target) {
+  // initialize starting ending and mid pointers of the array
+  var start = 0;
+  var end = array.length -1;
+  var midIndex = Math.round((start + end)/2);
+
+// define a function that recursively searches for the given target using divide and conquer approach
+  var searchForTarget = function(start, end, midIndex) {
+    if (target === array[midIndex]) {
+      return midIndex;
+    } else if (target === array[start]) {
+      return start;
+    } else if (midIndex >= end || midIndex <= start) {
+      return null;
+    } else {
+      if (target > array[midIndex]){
+        start = midIndex;
+        midIndex = Math.round((start + end)/2);
+        return searchForTarget(start, end, midIndex);
+      } else if (target < array[midIndex]) {
+        end = midIndex;
+        midIndex = Math.round((start + end)/2);
+        return searchForTarget(start, end, midIndex);
+      }
+    }
+  }
+  return searchForTarget(start, end, midIndex);
 };
 
