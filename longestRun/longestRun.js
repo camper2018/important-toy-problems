94a34cc841424db0dccbb5a507318766662b5bdc
Tania Ali <taniafali@gmail.com>
2019-07-25 15:22:15 -0700

    Add solution to longestRun

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -13,8 +13,46 @@
  * inputs well.
  */
 
+// ********************* HELPER FUNCTION ******************
+
+var createsRepeatStr = (str, times=1)=> {
+  let repeatedStr = '';
+  for (var i = 0; i < times; i++) {
+    repeatedStr+= str ;
+  }
+  return repeatedStr;
+}
+// *****************************************************
 var longestRun = function (string) {
   // TODO: Your code here!
+  if(string.length === 0) {
+    return null;
+  }
+  let longeststr = '';
+  let count = 0;
+  let charsSet= new Set(string.split(""));
+  let repeatChars = [...charsSet];
+  let longestRunIndices = [0, 0];
+  for (var i = 0; i < repeatChars.length; i++) {
+    for (var j = 0; j < string.length; j++) {
+      if (repeatChars[i] === string[j]) {
+        count++;
+        if(string[j] !== string[j + 1]){
+          count = 0;
+        } else {
+          let repeatedStr = createsRepeatStr(string[j], count + 1);
+          if (longeststr.length < repeatedStr.length ) {
+            longeststr = repeatedStr;
+            end = j + 1  ;
+            start = end - (longeststr.length - 1);
+            longestRunIndices = [start, end];
+          }
+        }
+      }
+    }
+  }
+  return longestRunIndices;
+
 };
 
 // If you need a random string generator, use this!
